e9630de4b688d6954a0ac6e599da1c96e579e4ec
Jaybatra <jaybatra73@gmail.com>
2022-03-23 20:07:59 +0530

    practice  Please enter the commit message for your changes. Lines starting

---
+++ matrixgforg.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std ;
+
+
+int main(){
+    int matrix[2][2]={1,7,13,20} ;
+
+    for(int i=0;i<2;i++){
+        for(int j=0;j<2;j++){
+        cout<<matrix[i][j]<<endl ;
+
+        }
+    }
+
+
+}
