44988358e2cebe56122fb67541d8491766993a4e
Jaybatra <jaybatra73@gmail.com>
2022-03-24 17:11:01 +0530

    practice  Please enter the commit message for your changes. Lines starting

---
+++ new.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std ;
+
+
+int main(){
+     int *a = new int(10) ;
+
+     int a[0] = 7 ;
+     int a[1] = 7 ;
+     int a[2] = 7 ;
+     int a[3] = 7 ;
+     int a[4] = 7 ;
+     int a[5] = 7 ;
+     int a[6] = 7 ;
+     int a[7] = 7 ;
+     int a[8] = 7 ;
+     int a[9] = 7 ;
+
+    for (int i=0;i<10;i++){
+        cout<<a[i]<<endl
+    }
+
+
+}
